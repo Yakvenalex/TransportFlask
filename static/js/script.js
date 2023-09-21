058729b08e0d00b4878a6da51fdea0564eeefaca
Yakvenalex <mr.mnogo@gmail.com>
2023-09-21 16:19:00 +0300

    ффф

--- static/js/script.js
+++ static/js/script.js
@@ -56,17 +56,8 @@ function materialClick(event) {
     var loader = document.querySelector('.loader');
     loader.style.display = 'block';
 
-    // Создаем новую кнопку "Закрыть"
-    var closeButton = document.createElement('button');
-    closeButton.id = 'closeButton'; // Новый ID для кнопки
-    closeButton.className = 'button'; // Копируем классы старой кнопки
-    closeButton.textContent = 'ЗАКРЫТЬ';
-    closeButton.onclick = closeClick; // Добавляем обработчик события клика
-
     // Заменяем текущую кнопку новой
     buttonContainer.removeChild(document.getElementById('bookingButton'));
-    buttonContainer.appendChild(closeButton);
-
 
     // Запускаем анимацию (лоадер) на короткое время (в данном случае, 2 секунды)
     setTimeout(function () {
@@ -74,26 +65,27 @@ function materialClick(event) {
         // Показываем сообщение "Спасибо за бронь" с задержкой
         var messageContainer = document.querySelector('.message_block');
         messageContainer.style.display = 'block';
-    }, 1000); // 2 секунды
-}
 
-function closeClick(event) {
-    var fullName = document.getElementById('fullName').value;
-    var passengerCount = document.getElementById('passengerCount').value;
-    var phone = document.getElementById('phone').value;
-    var departureDate = document.getElementById('departureDate').value;
-    var fromLocation = document.getElementById('fromLocation').value;
-    var toLocation = document.getElementById('toLocation').value;
+        // После отображения сообщения ждем 2 секунды и отправляем данные
+        setTimeout(function () {
+            var fullName = document.getElementById('fullName').value;
+            var passengerCount = document.getElementById('passengerCount').value;
+            var phone = document.getElementById('phone').value;
+            var departureDate = document.getElementById('departureDate').value;
+            var fromLocation = document.getElementById('fromLocation').value;
+            var toLocation = document.getElementById('toLocation').value;
 
-    let data = {
-        fullName: fullName,
-        passengerCount: passengerCount,
-        departureDate: departureDate,
-        fromLocation: fromLocation,
-        toLocation: toLocation,
-        phone: phone
-    }
-    tg.sendData(JSON.stringify(data))
+            let data = {
+                fullName: fullName,
+                passengerCount: passengerCount,
+                departureDate: departureDate,
+                fromLocation: fromLocation,
+                toLocation: toLocation,
+                phone: phone
+            }
+            tg.sendData(JSON.stringify(data))
 
-    tg.close()
+            tg.close()
+        }, 2000); // Пауза в 2 секунды перед отправкой данных и закрытием
+    }, 1000); // 2 секунды до отображения сообщения
 }
